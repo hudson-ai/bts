c5dd79455e43866f049a7e71a5033613d2725209
Hudson Cooper <hncooper96@gmail.com>
2023-11-30 15:50:48 -0800

    Package 😎

--- src/bts/__init__.py
+++ src/bts/__init__.py
@@ -35,17 +35,3 @@ def encode(u, n):
     l = encode_standard_basis(u, n)
     return pack(l)
 
-
-def test_pack_unpack():
-    for n in range(1, 1000):
-        assert n == pack(unpack(n))
-
-    for k in range(1, 63):
-        n = 2**k
-        assert n == pack(unpack(n))
-
-
-def test_unpack():
-    for n in range(1, 1000):
-        s = bin(n)[2:]
-        assert s == "".join([str(i) for i in unpack(n)])[: len(s)]
